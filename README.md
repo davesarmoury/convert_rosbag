5453a028f9d26ad659f5fcc89060f63cf8b0460f
Dave Niewinski <davesarmoury@gmail.com>
2022-04-07 14:27:08 -0400

    Updated to python3

---
+++ README.md
@@ -0,0 +1,5 @@
+Converting rosbag to CSV. Updated to python3
+
+Ported from: 
+
+https://www.clearpathrobotics.com/assets/guides/melodic/ros/Converting%20ROS%20bag%20to%20CSV.html
--- bag_to_csv.py
+++ bag_to_csv.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 This script saves each topic in a bagfile as a csv.
 
@@ -6,7 +7,7 @@ Accepts a filename as an optional argument. Operates on all bagfiles in current 
 Written by Nick Speal in May 2013 at McGill University's Aerospace Mechatronics Laboratory
 www.speal.ca
 
-Supervised by Professor Inna Sharf, Professor Meyer Nahon 
+Supervised by Professor Inna Sharf, Professor Meyer Nahon
 
 '''
 
@@ -15,33 +16,34 @@ import time
 import string
 import os #for file management make directory
 import shutil #for file management, copy file
+from tqdm import tqdm
 
 #verify correct input arguments: 1 or 2
 if (len(sys.argv) > 2):
-	print "invalid number of arguments:   " + str(len(sys.argv))
-	print "should be 2: 'bag2csv.py' and 'bagName'"
-	print "or just 1  : 'bag2csv.py'"
+	print("invalid number of arguments:   " + str(len(sys.argv)))
+	print("should be 2: 'bag2csv.py' and 'bagName'")
+	print("or just 1  : 'bag2csv.py'")
 	sys.exit(1)
 elif (len(sys.argv) == 2):
 	listOfBagFiles = [sys.argv[1]]
 	numberOfFiles = "1"
-	print "reading only 1 bagfile: " + str(listOfBagFiles[0])
+	print("reading only 1 bagfile: " + str(listOfBagFiles[0]))
 elif (len(sys.argv) == 1):
 	listOfBagFiles = [f for f in os.listdir(".") if f[-4:] == ".bag"]	#get list of only bag files in current dir.
 	numberOfFiles = str(len(listOfBagFiles))
-	print "reading all " + numberOfFiles + " bagfiles in current directory: \n"
+	print("reading all " + numberOfFiles + " bagfiles in current directory: \n")
 	for f in listOfBagFiles:
-		print f
-	print "\n press ctrl+c in the next 10 seconds to cancel \n"
+		print(f)
+	print("\n press ctrl+c in the next 10 seconds to cancel \n")
 	time.sleep(10)
 else:
-	print "bad argument(s): " + str(sys.argv)	#shouldnt really come up
+	print("bad argument(s): " + str(sys.argv))	#shouldnt really come up
 	sys.exit(1)
 
 count = 0
 for bagFile in listOfBagFiles:
 	count += 1
-	print "reading file " + str(count) + " of  " + numberOfFiles + ": " + bagFile
+	print("reading file " + str(count) + " of  " + numberOfFiles + ": " + bagFile)
 	#access bag
 	bag = rosbag.Bag(bagFile)
 	bagContents = bag.read_messages()
@@ -49,7 +51,7 @@ for bagFile in listOfBagFiles:
 
 
 	#create a new directory
-	folder = string.rstrip(bagName, ".bag")
+	folder = bagName.rstrip(".bag")
 	try:	#else already exists
 		os.makedirs(folder)
 	except:
@@ -66,20 +68,21 @@ for bagFile in listOfBagFiles:
 
 	for topicName in listOfTopics:
 		#Create a new CSV file for each topic
-		filename = folder + '/' + string.replace(topicName, '/', '_slash_') + '.csv'
+		print("Processing " + topicName)
+		filename = folder + '/' + topicName.replace('/', '_slash_') + '.csv'
 		with open(filename, 'w+') as csvfile:
 			filewriter = csv.writer(csvfile, delimiter = ',')
 			firstIteration = True	#allows header row
-			for subtopic, msg, t in bag.read_messages(topicName):	# for each instant in time that has data for topicName
+			for subtopic, msg, t in tqdm(bag.read_messages(topicName)):	# for each instant in time that has data for topicName
 				#parse data from this instant, which is of the form of multiple lines of "Name: value\n"
 				#	- put it in the form of a list of 2-element lists
 				msgString = str(msg)
-				msgList = string.split(msgString, '\n')
+				msgList = msgString.split('\n')
 				instantaneousListOfData = []
 				for nameValuePair in msgList:
-					splitPair = string.split(nameValuePair, ':')
+					splitPair = nameValuePair.split(':')
 					for i in range(len(splitPair)):	#should be 0 to 1
-						splitPair[i] = string.strip(splitPair[i])
+						splitPair[i] = splitPair[i].strip()
 					instantaneousListOfData.append(splitPair)
 				#write the first row from the first element of each pair
 				if firstIteration:	# header
@@ -95,4 +98,4 @@ for bagFile in listOfBagFiles:
 						values.append(pair[1])
 				filewriter.writerow(values)
 	bag.close()
-print "Done reading all " + numberOfFiles + " bag files."
+print("Done reading all " + numberOfFiles + " bag files.")
